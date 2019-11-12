3c37f31e015d17f1308f089dd9390da71b277550
Gabriel Colombo <gabriel.colombo@shx.com.br>
2019-11-12 09:53:13 -0300

    Scaffolding project structure

---
+++ src/__tests__/Button.spec.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+
+// describe('Button component', () => {
+//   test('Matches the provided label', () => {})
+// });
---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">Vrau</div>
+  );
+}
+
+export default App;
